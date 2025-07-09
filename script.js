15af1faa78e431cbfbbc01783b01d4563dde7421
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-07-09 10:25:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function makeid(l) {
-  // write your code here
+function makeid(length) {
+  const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = '';
+  const charactersLength = str.length;
+  for (let i = 0; i < length; i++) {
+    result += str.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
 }
 
 // Do not change the code below.
